f0baab3c6231ccd8827f0d6aba23346a23b884ab
Rohith09 <kandirohith09@gmail.com>
2018-04-14 17:10:57 +0530

    Bottom Sheet Commit

--- app/src/main/java/com/example/storytellerr/evhack/BottomSheetFragment.java
+++ app/src/main/java/com/example/storytellerr/evhack/BottomSheetFragment.java
@@ -1,20 +1,21 @@
 package com.example.storytellerr.evhack;
 
 
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.support.design.widget.BottomSheetDialogFragment;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RatingBar;
 import android.widget.ExpandableListAdapter;
 import android.widget.ExpandableListView;
 import android.widget.TextView;
 
 
-/**
- * A simple {@link Fragment} subclass.
- */
+
 public class BottomSheetFragment extends BottomSheetDialogFragment {
 
 
@@ -25,14 +26,25 @@ public class BottomSheetFragment extends BottomSheetDialogFragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+                             Bundle savedInstanceState)
+    {
         // Inflate the layout for this fragment
        View v= inflater.inflate(R.layout.fragment_bottom_sheet, container, false);
-       TextView tv=v.findViewById(R.id.name);
+        RatingBar rb = v.findViewById(R.id.ratings);
         Bundle mArgs = getArguments();
         String name = mArgs.getString("name");
-        String distance = mArgs.getString("distance");
-        tv.setText(name + distance);
+        String stars = mArgs.getString("stars");
+       // String phone = mArgs.getString("phone");
+        String add = mArgs.getString("address");
+        TextView shop_name = v.findViewById(R.id.shop_name);
+        TextView phone_no = v.findViewById(R.id.phone);
+        TextView address_shop = v.findViewById(R.id.address);
+        shop_name.setText(name);
+       // phone_no.setText(phone);
+        address_shop.setText(add);
+        rb.setNumStars(Integer.parseInt(stars));
+       // stars.getDrawable(2).setColorFilter(Color.YELLOW, PorterDuff.Mode.SRC_ATOP);
+
         return v;
     }
 
